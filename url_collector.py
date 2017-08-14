d4b6d7f336aca9824f4c932f80ec89a8db19be76
zhongxiao <729457563@qq.com>
2017-08-14 15:36:53 +0800

    Version 1.0 Done

--- url_collector.py
+++ url_collector.py
@@ -16,6 +16,13 @@ app.config.from_object(__name__)
 def connect_db():
     return mysql.connector.connect(user='zhong', password='xiao', database='url_collector')
 
+def pagenum(datalist, pageid):
+    display_num = 50
+    page_max = len(datalist)/display_num+1
+    start_num = (pageid - 1)*display_num
+    end_num = pageid * display_num
+    return (page_max, start_num, end_num, len(datalist))
+
 #每个请求的前置处理，连接数据库
 @app.before_request
 def before_request():
@@ -31,25 +38,30 @@ def teardown_request(exception):
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     session['anonymous']=True
+    '''
     g.cursor.execute('select domain,sub_class_id from urls order by second asc limit 10 ')
     domains = [dict(url=row[0], classid=row[1]) for row in g.cursor.fetchall()]
     print domains
     g.cursor.execute('select id,name from sub_class order by id asc')
     classes = [dict(id=row[0], name=row[1]) for row in g.cursor.fetchall()]
     return render_template('view_all.html', domains = domains, classes = classes)
+    '''
+    return render_template('layout.html')
 
 #已分类URL页面
-@app.route('/classified', methods = ['GET', 'POST'])
-def classified():
+@app.route('/classified/', methods = ['GET', 'POST'])
+@app.route('/classified/<int:pageid>', methods = ['GET', 'POST'])
+def classified(pageid=1):
     g.cursor.execute('select domain,sub_class_id from urls where sub_class_id != 120 order by second asc')
     domains = [dict(url=row[0], classid=row[1]) for row in g.cursor.fetchall()]
     g.cursor.execute('select id,name from sub_class order by id asc')
     classes = [dict(id=row[0], name=row[1]) for row in g.cursor.fetchall()]
+    pnum = pagenum(domains, pageid)
     update_count = 0
 
     if request.method == 'POST':
-        input_ids = [row['url'] for row in domains]
-        select_ids = [row['url']+'_id' for row in domains] 
+        input_ids = [row['url'] for row in domains[pnum[1]:pnum[2]]]
+        select_ids = [row['url']+'_id' for row in domains[pnum[1]:pnum[2]]] 
         for input_id,select_id in zip(input_ids, select_ids):
             if dict(url=request.form[input_id], classid=int(request.form[select_id])) not in domains:
 #                print 'update --> p1: %s' % dict(url=request.form[input_id], classid=request.form[select_id])
@@ -57,21 +69,27 @@ def classified():
                 g.db.commit()
                 update_count = update_count + 1
         flash('Update %d records!' % update_count)
-        return redirect(url_for('classified'))
-    return render_template('classified.html',domains = domains, classes = classes)
+        return redirect('/classified/')    
+    if (pageid > 0 and pageid <= pnum[0]):
+        return render_template('classified.html',pageid = pageid, page_max=pnum[0],total=pnum[3], domains = domains[pnum[1]:pnum[2]], classes = classes)
+    else:
+        flash('Invalid page')
+        return redirect('/classified/')
 
 #未分类URL页面
-@app.route('/classifying', methods = ['GET', 'POST'])
-def classifying():
+@app.route('/classifying/', methods = ['GET', 'POST'])
+@app.route('/classifying/<int:pageid>', methods = ['GET', 'POST'])
+def classifying(pageid=1):
     g.cursor.execute('select domain,sub_class_id from urls where sub_class_id = 120 order by second asc')
     domains = [dict(url=row[0], classid=row[1]) for row in g.cursor.fetchall()]
     g.cursor.execute('select id,name from sub_class order by id asc')
     classes = [dict(id=row[0], name=row[1]) for row in g.cursor.fetchall()]
+    pnum = pagenum(domains, pageid)
     update_count = 0
 
     if request.method == 'POST':
-        input_ids = [row['url'] for row in domains]
-        select_ids = [row['url']+'_id' for row in domains]  
+        input_ids = [row['url'] for row in domains[pnum[1]:pnum[2]]]
+        select_ids = [row['url']+'_id' for row in domains[pnum[1]:pnum[2]]]  
         for input_id,select_id in zip(input_ids, select_ids):
             if int(request.form[select_id]) != 120:
                 print 'update --> p1: %s -- p2 %s' % (request.form[select_id], request.form[input_id])
@@ -79,8 +97,12 @@ def classifying():
                 g.db.commit()
                 update_count = update_count + 1
         flash('Update %d records!' % update_count )
-        return redirect(url_for('classifying'))
-    return render_template('classifying.html',domains = domains, classes = classes)
+        return redirect('/classifying/')
+    if (pageid > 0 and pageid <= pnum[0]):
+        return render_template('classifying.html',pageid = pageid, page_max=pnum[0],total=pnum[3], domains = domains[pnum[1]:pnum[2]], classes = classes)
+    else:
+        flash('Invalid page')
+        return redirect('/classifying/')
 
 @app.route('/export', methods = ['GET'])
 def export():
